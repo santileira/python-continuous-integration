510ae3ae2e7dfba5814f36e7154f62b9449a7028
sleira <sleira@medallia.com>
2019-10-30 17:17:25 -0300

    Add functions for addition and substraction

---
+++ calculator.py
@@ -0,0 +1,9 @@
+"""
+Calculator library containing basic math operations.
+"""
+
+def add(first_term: int, second_term: int) -> int:
+    return first_term + second_term
+
+def subtract(first_term: int, second_term: int) -> int:
+    return first_term - second_term
